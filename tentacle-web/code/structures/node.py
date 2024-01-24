c49e30bb5c6380a0c3f7a5fc2c69ce1af4c34310
joao <Joao.Henares@btgpactual.com>
2024-01-24 15:39:55 -0500

    update on nodes

--- tentacle-web/code/structures/node.py
+++ tentacle-web/code/structures/node.py
@@ -1,3 +1,5 @@
+from nodelink import NodeLink
+
 class Node:
     def __init__(self):
         self.previous = set()       # set of NodeLink structures
@@ -26,10 +28,3 @@ class Node:
         self.previous.add(l)
         node.next.add(l)
 
-class NodeLink:
-    def __init__(self, n1=None, n2=None, descr=""):
-        self.n1 = n1          # node object
-        self.n2 = n2          # node object
-        # n1 and n2 order matters. n1 is the node that comes before n2.
-
-        self.descr = descr    # string
---
+++ tentacle-web/code/structures/nodelink.py
@@ -0,0 +1,7 @@
+class NodeLink:
+    def __init__(self, n1=None, n2=None, descr=""):
+        self.n1 = n1          # node object
+        self.n2 = n2          # node object
+        # n1 and n2 order matters. n1 is the node that comes before n2.
+
+        self.descr = descr    # string
