40bdf093070938cc6d1dab67264322420147dd38
joao <Joao.Henares@btgpactual.com>
2024-01-26 13:47:22 -0500

    adding database and api code

--- tentacle-web/code/structures/node.py
+++ tentacle-web/code/structures/node.py
@@ -8,6 +8,8 @@ from user import User
 # OR: we could use an ORM like SQLAlchemy to do this for us, so the objects will be
 # stored and we don't have to worry about it.
 
+from ..database.db_manager import Base
+
 class NodeId:
     def __init__(self):
         self.id = 0            # create an ID
